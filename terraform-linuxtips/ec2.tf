70155538e7c267a9dc6fff78272bfd20fe2d6bc1
rodrigocflorindo <rcflorindo@NOTE-RODRIGO.localdomain>
2022-01-11 11:26:24 -0300

    ok

---
+++ terraform-linuxtips/ec2.tf
@@ -0,0 +1,29 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Ubuntu
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform-Plan"
+    Env  = var.environment
+  }
+}
+
+resource "aws_eip" "ip" {
+  vpc      = true
+  instance = aws_instance.web.id
+}
+
+resource "aws_security_group" "elb_sg" {
+  # (resource arguments)
+}
